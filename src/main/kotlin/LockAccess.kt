fa7caa4ca18754c87af0db7e354543f7c6c3d972
Adam <897017+aSemy@users.noreply.github.com>
2025-05-16 16:07:42 +0100

    kdoc

--- src/main/kotlin/LockAccess.kt
+++ src/main/kotlin/LockAccess.kt
@@ -1,7 +1,32 @@
 package dev.adamko.advisoryfilelock
 
 sealed interface LockAccess : AutoCloseable {
+
+  /**
+   * Acquires the lock.
+   *
+   * Does nothing if the lock is already acquired.
+   *
+   * Blocks the current thread until the lock is acquired,
+   * or the thread is interrupted.
+   */
   fun lock()
+
+  /**
+   * Releases the lock.
+   *
+   * Might block the current thread until the lock is released,
+   * or the thread is interrupted.
+   *
+   * Does nothing if the lock is already released.
+   */
   fun unlock()
-  override fun close(): Unit = unlock()
+
+  /**
+   * Calls [unlock].
+   *
+   * Can be used as a resource in a `use {}` block.
+   */
+  override fun close(): Unit =
+    unlock()
 }
